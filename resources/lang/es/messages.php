c5042d2cd5f6ffb5726c754b8427ff307d140024
Luis Cabrera Benito <parzibyte@gmail.com>
2020-07-14 20:17:43 -0500

    Traducción al español

---
+++ resources/lang/es/messages.php
@@ -0,0 +1,16 @@
+<?php
+return [
+    "add_category" => "Agregar categoría",
+    "categories" => "Categorías",
+    "name" => "Nombre",
+    "description" => "Descripción",
+    "category_created" => "Categoría creada",
+    "save" => "Guardar",
+    "go_back" => "Volver",
+    "edit" => "Editar",
+    "delete" => "Eliminar",
+    "edit_category" => "Editar categoría",
+    "confirm_action" => "Por favor, confirme",
+    "category_updated" => "Categoría actualizada",
+    "category_deleted" => "Categoría eliminada",
+];
