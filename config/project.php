18cb48370934c8cce47b4a136a5eb4f3930caa74
Luis Cabrera Benito <parzibyte@gmail.com>
2020-07-16 12:16:00 -0500

    El directorio para las imágenes de productos se toma de la configuración; pues hay problemas de concurrencia con .env

---
+++ config/project.php
@@ -0,0 +1,8 @@
+<?php
+return [
+    /* Project settings. I couldn't put in the .env because there are issues with concurrent access to file.
+    See more: https://stackoverflow.com/questions/30982441/laravel-5-0-env-returns-null-during-concurrent-requests
+    */
+    'pictures_directory' => 'pictures',
+
+];
